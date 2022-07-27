ee475781af66b0f660bf5c1e3d0c92419a691e1a
Akshita Jain <akshitajain1906@gmail.com>
2022-07-27 17:31:35 +0530

    added buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById("play").addEventListener("click", play);
+document.getElementById("reset").addEventListener("click", reset);
+
+function play(){
 //rand no between 1 to 6
 var rand1 = Math.floor(Math.random()*6+1);
 var rand2 = Math.floor(Math.random()*6+1);
@@ -5,10 +9,14 @@ var rand2 = Math.floor(Math.random()*6+1);
 document.getElementById("1").setAttribute("src","images/dice"+rand1+".png")
 document.getElementById("2").src="images/dice"+rand2+".png"
 if(rand1 > rand2){
-    document.gquerySelector("h1") .innerHTML="Winner:Player 1!!";
+    document.querySelector("h1") .innerHTML="Winner:Player1!!";
 }
 else if(rand1==rand2)
     document.querySelector("h1").innerHTML="Its draw";
 else
     document.querySelector("h1").innerHTML="Winner:Player2!!";
- 
+}
+
+function reset(){
+document.getElementById("1").setAttribute("src","images/dice6.png")
+}
